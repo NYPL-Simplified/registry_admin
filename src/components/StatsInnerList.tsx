e27dcc3ab0c4e06c87ab0912c22bb3fe68464a84
Adriana Alter <adrianaalter@nypl.org>
2020-02-11 16:34:52 -0500

    Remove unnecessary months array

--- src/components/StatsInnerList.tsx
+++ src/components/StatsInnerList.tsx
@@ -57,7 +57,6 @@ export default class StatsInnerList extends React.Component<StatsInnerListProps,
     if (!this.props.showMonths) {
       return "";
     }
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     return ` (${getMonth(library.basic_info.timestamp)})`;
   }
 
