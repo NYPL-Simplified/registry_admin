29c7a391f4f2065704ded686f3811f89a0785caf
Olivia Wong <oliviawong@nypl.org>
2021-07-28 11:52:34 -0400

    Modifies so panels only render content on click

--- src/components/LibrariesListItem.tsx
+++ src/components/LibrariesListItem.tsx
@@ -1,70 +1,91 @@
-import * as React from "react";
-import { Panel } from "library-simplified-reusable-components";
-import { Store } from "redux";
-import { State } from "../reducers/index";
-import { LibraryData } from "../interfaces";
-import LibraryDetailPage from "./LibraryDetailPage";
+import * as React from 'react'
+import { Panel } from 'library-simplified-reusable-components'
+import { Store } from 'redux'
+import { State } from '../reducers/index'
+import { LibraryData } from '../interfaces'
+import LibraryDetailPage from './LibraryDetailPage'
 
 export interface LibrariesListItemProps {
-  library: LibraryData;
-  store: Store<State>;
+  library: LibraryData
+  store: Store<State>
 }
 
 export interface LibrariesListItemState {
-  color: string;
+  color: string
+  panelOpen: boolean
 }
 
 /** A color-coded panel displayed for each item in LibraryList, containing a LibraryDetailPage. */
-export default class LibrariesListItem extends React.Component<LibrariesListItemProps, LibrariesListItemState> {
+export default class LibrariesListItem extends React.Component<
+  LibrariesListItemProps,
+  LibrariesListItemState
+> {
   constructor(props: LibrariesListItemProps) {
-    super(props);
-    const color = this.colorCode(Object.values(this.props.library.stages));
-    this.state = { color };
-    this.colorCode = this.colorCode.bind(this);
-    this.updateColor = this.updateColor.bind(this);
-    this.content = this.content.bind(this);
+    super(props)
+    const color = this.colorCode(Object.values(this.props.library.stages))
+    this.state = { color, panelOpen: false }
+    this.colorCode = this.colorCode.bind(this)
+    this.updateColor = this.updateColor.bind(this)
+    this.content = this.content.bind(this)
+    this.handlePanelClick = this.handlePanelClick.bind(this)
+  }
+
+  handlePanelClick() {
+    console.log('in handlepanelclick')
+    this.setState((prevState) => ({
+      ...prevState,
+      panelOpen: !prevState.panelOpen,
+    }))
   }
 
   content() {
     return (
-        <LibraryDetailPage
-          library={this.props.library}
-          updateColor={this.updateColor}
-          store={this.props.store}
-        />
-    );
+      <LibraryDetailPage
+        library={this.props.library}
+        updateColor={this.updateColor}
+        store={this.props.store}
+      />
+    )
   }
 
   /**
-  * Passed as a callback to the child LibraryDetailPage component, so that the LibrariesListItem
-  * background color will update to reflect changes to the library's stages.
-  */
+   * Passed as a callback to the child LibraryDetailPage component, so that the LibrariesListItem
+   * background color will update to reflect changes to the library's stages.
+   */
   updateColor(stages: Array<string>): void {
-      let color = this.colorCode(stages);
-      this.setState({ color });
+    let color = this.colorCode(stages)
+    this.setState({ color })
   }
 
   /**
-  *If both library_stage and registry_stage are in production, background is green;
-  * if at least one of them is cancelled, background is red;
-  * otherwise, background is yellow.
-  */
+   *If both library_stage and registry_stage are in production, background is green;
+   * if at least one of them is cancelled, background is red;
+   * otherwise, background is yellow.
+   */
   colorCode(stages: Array<string>): string {
-    if (stages.every((stage) => stage === "production")) {
-      return "success";
-    } else if (stages.some((stage) => stage === "cancelled")) {
-      return "danger";
+    if (stages.every((stage) => stage === 'production')) {
+      return 'success'
+    } else if (stages.some((stage) => stage === 'cancelled')) {
+      return 'danger'
     }
-    return "warning";
+    return 'warning'
   }
 
   render(): JSX.Element {
-    let style = this.state.color;
-    let { name } = this.props.library.basic_info;
-    return(
+    console.log('this.state.panelOpen', this.state.panelOpen)
+    let style = this.state.color
+    let { name } = this.props.library.basic_info
+    return (
       <li>
-        <Panel id={name.replace(/ /g, "-")} style={style} headerText={name} content={this.content()} />
+        <div onClick={this.handlePanelClick}>
+          <Panel
+            id={name.replace(/ /g, '-')}
+            style={style}
+            headerText={name}
+            content={this.state.panelOpen ? this.content() : ''}
+          />
+        </div>
       </li>
-    );
+    )
   }
 }
--- src/components/LibrariesPage.tsx
+++ src/components/LibrariesPage.tsx
@@ -33,10 +33,16 @@ export interface LibrariesPageState {
   filters: { [key: string]: any };
 }
 
-export interface LibrariesPageProps extends LibrariesPageStateProps, LibrariesPageOwnProps, LibrariesPageDispatchProps {}
+export interface LibrariesPageProps
+  extends LibrariesPageStateProps,
+    LibrariesPageOwnProps,
+    LibrariesPageDispatchProps {}
 
 /** The main component containing both the Aggregate Data panel and the LibrariesList. */
-export class LibrariesPage extends React.Component<LibrariesPageProps, LibrariesPageState> {
+export class LibrariesPage extends React.Component<
+  LibrariesPageProps,
+  LibrariesPageState
+> {
   constructor(props: LibrariesPageProps) {
     super(props);
     this.search = this.search.bind(this);
@@ -46,7 +52,12 @@ export class LibrariesPage extends React.Component<LibrariesPageProps, Libraries
     this.setFilter = this.setFilter.bind(this);
     this.filterByAttribute = this.filterByAttribute.bind(this);
     this.flipFilter = this.flipFilter.bind(this);
-    this.state = { showAll: true, searchTerm: "", qa: false, filters: { flipped: false, attributes: { "PLS ID": false }} };
+    this.state = {
+      showAll: true,
+      searchTerm: "",
+      qa: false,
+      filters: { flipped: false, attributes: { "PLS ID": false } },
+    };
   }
 
   convertToAttrName(name: string): string {
@@ -56,29 +67,41 @@ export class LibrariesPage extends React.Component<LibrariesPageProps, Libraries
   render(): JSX.Element {
     let libraries = this.getLibraryList();
 
-    let searchForm: JSX.Element = <SearchForm
-      search={this.search}
-      term={this.state.searchTerm}
-      text="Search"
-      inputName="name"
-      clear={!this.state.showAll ? this.clear : null}
-      resultsCount={libraries && libraries.length}
-    />;
+    let searchForm: JSX.Element = (
+      <SearchForm
+        search={this.search}
+        term={this.state.searchTerm}
+        text="Search"
+        inputName="name"
+        clear={!this.state.showAll ? this.clear : null}
+        resultsCount={libraries && libraries.length}
+      />
+    );
 
-    let filter: JSX.Element = <Filter
-      filterKeys={this.state.filters.attributes}
-      setFilter={this.setFilter}
-      flipFilter={this.flipFilter}
-      title="Only show libraries which"
-    />;
+    let filter: JSX.Element = (
+      <Filter
+        filterKeys={this.state.filters.attributes}
+        setFilter={this.setFilter}
+        flipFilter={this.flipFilter}
+        title="Only show libraries which"
+      />
+    );
 
-    let toggle: JSX.Element = <Toggle onToggle={this.toggleQA} initialOn={this.state.qa} label="Show All" />;
+    let toggle: JSX.Element = (
+      <Toggle
+        onToggle={this.toggleQA}
+        initialOn={this.state.qa}
+        label="Show All"
+      />
+    );
     return (
       <div className="libraries-page">
-        { toggle }
-        <Stats libraries={this.props.libraries && this.props.libraries.libraries} />
-        { searchForm }
-        { filter }
+        {toggle}
+        <Stats
+          libraries={this.props.libraries && this.props.libraries.libraries}
+        />
+        {searchForm}
+        {filter}
         <LibrariesList
           libraries={libraries}
           store={this.props.store}
@@ -94,80 +117,106 @@ export class LibrariesPage extends React.Component<LibrariesPageProps, Libraries
 
   updateLibraryList(libraryList) {
     let newLibrary = this.props.updatedLibrary;
-    let oldLibraryIdx = libraryList.findIndex(l => l.uuid === newLibrary.uuid);
+    let oldLibraryIdx = libraryList.findIndex(
+      (l) => l.uuid === newLibrary.uuid
+    );
     libraryList[oldLibraryIdx] = newLibrary;
     return libraryList;
   }
 
   filterProduction(libraries) {
-    let inProduction = (l) => Object.values(l.stages).every(s => s === "production");
-    return libraries.filter(l => inProduction(l));
+    let inProduction = (l) =>
+      Object.values(l.stages).every((s) => s === "production");
+    return libraries.filter((l) => inProduction(l));
   }
 
   /**
-  * @param {string} filter The library attribute to be filtered by
-  */
+   * @param {string} filter The library attribute to be filtered by
+   */
   setFilter(filter: string) {
     let updatedFilters = this.state.filters.attributes;
     updatedFilters[filter] = !this.state.filters.attributes[filter];
-    this.setState({ filters: {flipped: this.state.filters.flipped, attributes: updatedFilters} });
+    this.setState({
+      filters: {
+        flipped: this.state.filters.flipped,
+        attributes: updatedFilters,
+      },
+    });
   }
 
   /**
-  * Switch between showing libraries which have the specified attributes and showing libraries which do not
-  * have them.
-  */
+   * Switch between showing libraries which have the specified attributes and showing libraries which do not
+   * have them.
+   */
   flipFilter() {
-    this.setState({ filters: {flipped: !this.state.filters.flipped, attributes: this.state.filters.attributes} });
+    this.setState({
+      filters: {
+        flipped: !this.state.filters.flipped,
+        attributes: this.state.filters.attributes,
+      },
+    });
   }
 
-
   /**
-  * Recursively check whether a library has a truthy value for a particular attribute
-  */
+   * Recursively check whether a library has a truthy value for a particular attribute
+   */
   hasAttr(dict, attr) {
-    let isDict = (dict) => dict && typeof(dict) === "object" && !Array.isArray(dict);
+    let isDict = (dict) =>
+      dict && typeof dict === "object" && !Array.isArray(dict);
     if (!isDict(dict)) {
       return;
     }
     if (Object.keys(dict).indexOf(attr) >= 0) {
       return !!dict[attr];
     }
-    let subDicts = Object.values(dict).filter(v => isDict(v));
+    let subDicts = Object.values(dict).filter((v) => isDict(v));
     return !!subDicts.filter((sd) => this.hasAttr(sd, attr)).length;
   }
 
   /**
-  * For each attribute that the user wants to filter by, narrow down the list of libraries to display.
-  */
+   * For each attribute that the user wants to filter by, narrow down the list of libraries to display.
+   */
   filterByAttribute(libraries) {
-    let attributes = Object.keys(this.state.filters.attributes).filter(attr => this.state.filters.attributes[attr]);
-    libraries && attributes.forEach(attr => {
-      attr = this.convertToAttrName(attr);
-      libraries = libraries.filter(l => this.state.filters.flipped ? !this.hasAttr(l, attr) : this.hasAttr(l, attr));
-    });
+    let attributes = Object.keys(this.state.filters.attributes).filter(
+      (attr) => this.state.filters.attributes[attr]
+    );
+    libraries &&
+      attributes.forEach((attr) => {
+        attr = this.convertToAttrName(attr);
+        libraries = libraries.filter((l) =>
+          this.state.filters.flipped
+            ? !this.hasAttr(l, attr)
+            : this.hasAttr(l, attr)
+        );
+      });
     return libraries;
   }
 
   getLibraryList() {
     // Does the registry have libraries?
-    let hasLibraries = (this.props.libraries && this.props.libraries.libraries && this.props.libraries.libraries.length);
+    let hasLibraries =
+      this.props.libraries &&
+      this.props.libraries.libraries &&
+      this.props.libraries.libraries.length;
     // List of all libraries for the registry:
-    let allLibraries =  hasLibraries && this.props.libraries.libraries;
+    let allLibraries = hasLibraries && this.props.libraries.libraries;
     // The user edited a library:
     if (allLibraries && this.props.updatedLibrary) {
       allLibraries = this.updateLibraryList(allLibraries);
     }
     // There might be QA libraries available, but the user only wants to see the production ones:
-    let filteredLibraries = !this.state.qa && allLibraries && this.filterProduction(allLibraries);
+    let filteredLibraries =
+      !this.state.qa && allLibraries && this.filterProduction(allLibraries);
     // The user has submitted a search, and there are results:
-    let allResults = !this.state.showAll && this.props.results && this.props.results.libraries;
+    let allResults =
+      !this.state.showAll && this.props.results && this.props.results.libraries;
     // The user edited a search result:
     if (allResults && this.props.updatedLibrary) {
       allResults = this.updateLibraryList(allResults);
     }
     // The user has submitted a search, but only wants to see results for production libraries:
-    let filteredResults = !this.state.qa && allResults && this.filterProduction(allResults);
+    let filteredResults =
+      !this.state.qa && allResults && this.filterProduction(allResults);
     // The user has submitted a search, but there are no results:
     let noResults = !this.state.showAll && !this.props.results && [];
 
@@ -186,22 +235,31 @@ export class LibrariesPage extends React.Component<LibrariesPageProps, Libraries
 
     // If one of the libraries undergoes changes--e.g. is put into/taken out of production--the libraries variable will be updated accordingly.
 
-    let list = (filteredResults || allResults || noResults) || filteredLibraries || allLibraries;
+    let list =
+      filteredResults ||
+      allResults ||
+      noResults ||
+      filteredLibraries ||
+      allLibraries;
     return this.filterByAttribute(list);
   }
 
   async toggleQA(showQA: boolean) {
-    this.setState({ "qa": showQA });
+    this.setState({ qa: showQA });
     if (showQA && this.state.searchTerm && !this.props.results) {
       // The user searched for a QA library, but didn't switch to QA mode until after submitting the search.
-      let searchTerm = new (window as any).FormData;
+      let searchTerm = new (window as any).FormData();
       searchTerm.append("name", this.state.searchTerm);
       await this.search(searchTerm);
     }
   }
 
   async search(data: FormData) {
-    this.setState({ showAll: false, searchTerm: (data.get("name") as string), filters: this.state.filters });
+    this.setState({
+      showAll: false,
+      searchTerm: data.get("name") as string,
+      filters: this.state.filters,
+    });
     await this.props.search(data);
   }
 
@@ -216,19 +274,26 @@ function mapStateToProps(state: State, ownProps: LibrariesPageOwnProps) {
     libraries: state.libraries && state.libraries.data,
     updatedLibrary: state.library && state.library.data,
     results: state.results && state.results.data,
-    isLoaded: state.libraries && state.libraries.isLoaded
+    isLoaded: state.libraries && state.libraries.isLoaded,
   };
 }
 
-function mapDispatchToProps(dispatch: Function, ownProps: LibrariesPageOwnProps) {
+function mapDispatchToProps(
+  dispatch: Function,
+  ownProps: LibrariesPageOwnProps
+) {
   let actions = new ActionCreator(null);
   return {
     fetchQA: () => dispatch(actions.fetchLibraries("/qa")),
-    search: (data: FormData) => dispatch(actions.search(data))
+    search: (data: FormData) => dispatch(actions.search(data)),
   };
 }
 
-const ConnectedLibrariesPage = connect<LibrariesPageStateProps, LibrariesPageDispatchProps, LibrariesPageOwnProps>(
+const ConnectedLibrariesPage = connect<
+  LibrariesPageStateProps,
+  LibrariesPageDispatchProps,
+  LibrariesPageOwnProps
+>(
   mapStateToProps,
   mapDispatchToProps
 )(LibrariesPage);
