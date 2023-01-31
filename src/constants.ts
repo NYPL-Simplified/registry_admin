fd9bceef75de95901bde4878e354b54556b9385e
Samantha Andrews <34348332+samanthaandrews@users.noreply.github.com>
2023-01-31 11:21:07 -0700

    Fix library endpoint to be `/libraries/qa` even in prod (#101)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 export const LOGIN = `${process.env.REGISTRY_API_DOMAIN}/log_in/jwt`;
 export const REFRESH = `${process.env.REGISTRY_API_DOMAIN}/refresh_token`;
-export const FETCH_LIBRARIES = process.env.REGISTRY_API_DOMAIN?.includes('qa')
-  ? `${process.env.REGISTRY_API_DOMAIN}/libraries/qa`
-  : `${process.env.REGISTRY_API_DOMAIN}/libraries`;
+// despite this endpoint including /qa, it is indeed supposed to be used in production :'(
+export const FETCH_LIBRARIES = `${process.env.REGISTRY_API_DOMAIN}/libraries/qa`;
 export const UPDATE_LIBRARY_STAGE = `${process.env.REGISTRY_API_DOMAIN}/libraries/registration`;
